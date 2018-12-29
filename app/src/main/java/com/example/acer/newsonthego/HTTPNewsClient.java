31f3e6921736bbb74ef075a27943298ac709c181
itsSiddharthGupta <36455409+itsSiddharthGupta@users.noreply.github.com>
2018-12-30 00:39:19 +0530

    Add HTTPNewsClient xml file and Nesw_Stuff java file

---
+++ app/src/main/java/com/example/acer/newsonthego/HTTPNewsClient.java
@@ -0,0 +1,51 @@
+package com.example.acer.newsonthego;
+
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class HTTPNewsClient {
+    private static String BASE_URL = "https://newsapi.org/v2/top-headlines?country=in&category=" +
+                                     "business&apiKey=d0f03357767249dba9395c94bfa3e1a9";
+
+    public static String getNewsStuff(){
+        HttpURLConnection httpURLConnection = null;
+        InputStream inputStream = null;
+
+        try{
+            httpURLConnection = (HttpURLConnection)(new URL(BASE_URL)).openConnection();
+            httpURLConnection.setRequestMethod("GET");
+            httpURLConnection.setDoInput(true);
+            httpURLConnection.connect();
+
+            StringBuffer stringBuffer = new StringBuffer();
+            inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = null;
+            while((line = bufferedReader.readLine())!= null){
+                stringBuffer.append(line + "\n");
+            }
+
+            inputStream.close();
+            httpURLConnection.disconnect();
+
+            return stringBuffer.toString();
+        }catch (Throwable t){
+            t.printStackTrace();
+        }finally {
+            try {
+                inputStream.close();
+                httpURLConnection.disconnect();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+}
---
+++ app/src/main/java/com/example/acer/newsonthego/News_Stuff.java
@@ -0,0 +1,49 @@
+package com.example.acer.newsonthego;
+
+public class News_Stuff {
+    private String title ;
+    private String headline ;
+    private String author ;
+    private String date ;
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getHeadline() {
+        return headline;
+    }
+
+    public void setHeadline(String headline) {
+        this.headline = headline;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
