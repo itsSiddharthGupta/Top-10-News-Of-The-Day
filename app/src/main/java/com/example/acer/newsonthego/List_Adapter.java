315a0b48952867582e51da9de52d188987f44422
itsSiddharthGupta <36455409+itsSiddharthGupta@users.noreply.github.com>
2018-12-30 00:43:36 +0530

    Use an ArrayList of News_Stuff object to display each item

--- app/src/main/java/com/example/acer/newsonthego/List_Adapter.java
+++ app/src/main/java/com/example/acer/newsonthego/List_Adapter.java
@@ -1,6 +1,8 @@
 package com.example.acer.newsonthego;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,21 +15,27 @@ public class List_Adapter extends BaseAdapter {
 
     Context context;
     LayoutInflater layoutInflater;
-    String[] news = {"news 1","news 2","news 3","news 4","news 5","news 6","news 7","news 8","news 9","news 10"};
+    TextView txtTitle;
+    TextView txtAuthor;
+    TextView txtDescription;
+    TextView txtDate;
+    ArrayList<News_Stuff> news ;
+    //String[] news = new String[10];
 
-    public List_Adapter(Context context){
+    public List_Adapter(Context context,ArrayList<News_Stuff> arrayList){
         this.context = context;
+        news = arrayList;
         layoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
     public int getCount() {
-        return news.length;
+        return news.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return news[position];
+        return news.get(position);
     }
 
     @Override
@@ -38,10 +46,58 @@ public class List_Adapter extends BaseAdapter {
     @Override
     public View getView(int position, View view, ViewGroup parent) {
         view = layoutInflater.inflate(R.layout.list_view,null);
-        TextView txtTitle = view.findViewById(R.id.txtTitle);
-        TextView txtAuthor = view.findViewById(R.id.txtAuthor);
-        TextView txtDescription = view.findViewById(R.id.txtDescription);
-        TextView txtDate = view.findViewById(R.id.txtDate);
+        txtTitle = view.findViewById(R.id.txtTitle);
+        txtAuthor = view.findViewById(R.id.txtAuthor);
+        txtDescription = view.findViewById(R.id.txtDescription);
+        txtDate = view.findViewById(R.id.txtDate);
+        txtTitle.setText(news.get(position).getTitle());
+        txtAuthor.setText(news.get(position).getAuthor());
+        txtDescription.setText(news.get(position).getHeadline());
+        txtDate.setText(news.get(position).getDate());
         return view;
     }
+
+
+
+    /*private class JSONNewsStuffTask extends AsyncTask<String,Void,News_Stuff> {
+        Context context;
+        ProgressDialog progressDialog;
+        int position;
+        public JSONNewsStuffTask(Context context,int position){
+            this.context = context;
+            this.position = position;
+            progressDialog = new ProgressDialog(context);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog.setTitle("Please wait while the news are fetching...");
+        }
+
+        @Override
+        protected News_Stuff doInBackground(String... strings) {
+            News_Stuff news_stuff = new News_Stuff();
+            String data = ((new HTTPNewsClient()).getNewsStuff());
+            try{
+                news_stuff = JSON_newsParser.get_News_Stuff(data,position);
+            }catch (Throwable t){
+                t.printStackTrace();
+            }
+            return news_stuff;
+        }
+
+        @Override
+        protected void onPostExecute(News_Stuff news_stuff) {
+            super.onPostExecute(news_stuff);
+            txtTitle.setText(news_stuff.getTitle());
+            txtAuthor.setText(news_stuff.getAuthor());
+            txtDescription.setText(news_stuff.getHeadline());
+            txtDate.setText(news_stuff.getDate());
+
+            if(progressDialog.isShowing()){
+                progressDialog.dismiss();
+            }
+        }
+    }*/
 }
