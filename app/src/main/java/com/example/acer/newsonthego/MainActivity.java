24ea6d45c1f4b4f1863251c4672eb14941dfb664
itsSiddharthGupta <36455409+itsSiddharthGupta@users.noreply.github.com>
2018-12-30 00:45:02 +0530

    Create an inner class to perform background fetching of news

--- app/src/main/java/com/example/acer/newsonthego/MainActivity.java
+++ app/src/main/java/com/example/acer/newsonthego/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.acer.newsonthego;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -9,14 +12,64 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ListView newsList;
-
+    int count;
+    ArrayList<News_Stuff> news;
+    /*
+    * Now i want to create a layout for making user to click the category for which he wants to view the news
+    */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         newsList = findViewById(R.id.newsList);
-        List_Adapter list_adapter = new List_Adapter(MainActivity.this);
-        newsList.setAdapter(list_adapter);
+        count = 0;
+        news = new ArrayList<News_Stuff>();
+        JSONNewsStuffTask jsonNewsStuffTask = new JSONNewsStuffTask(MainActivity.this);
+        jsonNewsStuffTask.execute();
 
+
+    }
+    private class JSONNewsStuffTask extends AsyncTask<String,Void,News_Stuff> {
+        Context context;
+        ProgressDialog progressDialog;
+
+        public JSONNewsStuffTask(Context context){
+            this.context = context;
+            progressDialog = new ProgressDialog(context);
+
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog.setTitle("Please wait while the news are fetching...");
+            progressDialog.show();
+        }
+
+        @Override
+        protected News_Stuff doInBackground(String... strings) {
+            News_Stuff news_stuff = new News_Stuff();
+            String data = ((new HTTPNewsClient()).getNewsStuff());
+            try{
+                for(count = 0;count<10;count++) {
+                    news_stuff = new News_Stuff();
+                    news_stuff = JSON_newsParser.get_News_Stuff(data,count);
+                    news.add(news_stuff);
+                }
+            }catch (Throwable t){
+                t.printStackTrace();
+            }
+            return news_stuff;
+        }
+
+        @Override
+        protected void onPostExecute(News_Stuff news_stuff) {
+            super.onPostExecute(news_stuff);
+            List_Adapter list_adapter = new List_Adapter(MainActivity.this,news);
+            newsList.setAdapter(list_adapter);
+            if(progressDialog.isShowing()){
+                progressDialog.dismiss();
+            }
+        }
     }
 }
