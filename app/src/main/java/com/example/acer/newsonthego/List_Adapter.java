6fd9baa1e9d3805d5f6ccfb343e0beff19959a0a
itsSiddharthGupta <36455409+itsSiddharthGupta@users.noreply.github.com>
2019-01-06 22:40:08 +0530

    Add colors to listview.

--- app/src/main/java/com/example/acer/newsonthego/List_Adapter.java
+++ app/src/main/java/com/example/acer/newsonthego/List_Adapter.java
@@ -2,11 +2,14 @@ package com.example.acer.newsonthego;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.AsyncTask;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -19,8 +22,11 @@ public class List_Adapter extends BaseAdapter {
     TextView txtAuthor;
     TextView txtDescription;
     TextView txtDate;
-    ArrayList<News_Stuff> news ;
-    //String[] news = new String[10];
+    ArrayList<News_Stuff> news;
+    RelativeLayout relativeLayout;
+    int[] newsBackground = {R.color.newsBackground1,R.color.newsBackground2,R.color.newsBackground3,R.color.newsBackground4
+            ,R.color.newsBackground5,R.color.newsBackground6,R.color.newsBackground7,R.color.newsBackground8
+            ,R.color.newsBackground9,R.color.newsBackground10};
 
     public List_Adapter(Context context,ArrayList<News_Stuff> arrayList){
         this.context = context;
@@ -54,6 +60,9 @@ public class List_Adapter extends BaseAdapter {
         txtAuthor.setText(news.get(position).getAuthor());
         txtDescription.setText(news.get(position).getHeadline());
         txtDate.setText(news.get(position).getDate());
+        relativeLayout = view.findViewById(R.id.relativeLayout);
+        relativeLayout.setBackgroundResource(newsBackground[position]);
+        Log.i("Position " + position,"color : "+ newsBackground[position]);
         return view;
     }
 
